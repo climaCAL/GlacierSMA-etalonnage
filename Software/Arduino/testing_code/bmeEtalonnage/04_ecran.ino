6a57a110bf7be3fc7ba59ef46576dc266966544b
Yanick H <yheynema@gmail.com>
2023-11-13 13:43:40 -0500

    Revision 1
    Detachement avec la cryologger

--- Software/Arduino/testing_code/bmeEtalonnage/04_ecran.ino
+++
@@ -1,7 +0,0 @@
-void data2Screen(DateTime rtcTime) {
-  monEcran.effacer();
-  monEcran.ecrire("Acq. No. " + String(acquisitionNumber), 0);
-  monEcran.ecrire("Time " + String(rtcTime.hour()) + ':' + String(rtcTime.minute()) + ':' + String(rtcTime.second()) , 1);
-  monEcran.ecrire("TEMP(°C): " + String(bme.readTemperature()), 2);
-  monEcran.ecrire("HUM(%): " + String(bme.readHumidity()), 3);
-}
--- Software/README.md
+++
@@ -1,6 +0,0 @@
-# Cryologger - Automatic Weather Station: Software
-
-## Repository Contents
-
-* **/Arduino** - Code uploaded to the Cryologger to power the weather station.
-* **/Python** - Python code used for analysis of weather data collected by the Cryologger.
